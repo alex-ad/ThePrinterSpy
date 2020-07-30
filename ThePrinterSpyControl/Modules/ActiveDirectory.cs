672fc1fe76452e9d58a95783b115bb01a5f6cfa2
Alexandr Grigoriev <grigoriev.a.v.1980@gmail.com>
2020-07-30 19:34:01 +0400

    v.1.0

---
+++ ThePrinterSpyControl/Modules/ActiveDirectory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThePrinterSpyControl.Models;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+
+namespace ThePrinterSpyControl.Modules
+{
+    class ActiveDirectory
+    {
+        private readonly UserNode usersNode;
+        private readonly AdIdentity _identity;
+
+        public ActiveDirectory(AdIdentity ad) {
+            _identity = ad;
+            usersNode = new UserNode();
+        }
+
+        public void GetUsers()
+        {
+            PrincipalContext oPrincipalContext = GetPrincipalContext();
+        }
+
+        private PrincipalContext GetPrincipalContext()
+        {
+            return new PrincipalContext(ContextType.Domain, _identity.Server, _identity.Dn, _identity.User, _identity.Password);
+        }
+    }
+}
